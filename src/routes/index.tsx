c967c2f36ec5ee4dbcda1503724d89df9ccd5525
Jeff Njoroge <60646637+jeff283@users.noreply.github.com>
2025-12-08 02:12:10 +0300

    Implement server-side loading of todos in the main route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,27 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { createServerFn } from '@tanstack/react-start'
+import { db } from '@/db'
 
-export const Route = createFileRoute('/')({ component: App })
+const todosFn = createServerFn({ method: 'GET' }).handler(async () => {
+  const todos = await db.query.todos.findMany()
+  return todos
+})
+
+export const Route = createFileRoute('/')({
+  component: App,
+  loader: () => todosFn(),
+})
 
 function App() {
-  return <div>Hello</div>
+  const todos = Route.useLoaderData()
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.name}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
