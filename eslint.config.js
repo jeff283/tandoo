5eb7e6fe065727ab0f2dcd49a39db98bf55a07b2
Jeff Njoroge <60646637+jeff283@users.noreply.github.com>
2025-12-08 01:35:23 +0300

    Refactor ESLint configuration to include custom rules and maintain structure

--- eslint.config.js
+++ eslint.config.js
@@ -2,4 +2,17 @@
 
 import { tanstackConfig } from '@tanstack/eslint-config'
 
-export default [...tanstackConfig]
+const eslintConfig = [
+  ...tanstackConfig,
+  //   {
+  //     ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+  //   },
+  {
+    rules: {
+      // Custom rules can be added here
+      'sort-imports': 'off',
+    },
+  },
+]
+
+export default eslintConfig
