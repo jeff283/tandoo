89fc8bc7648fd5be652ad79e787bf1c5ad8cee54
Jeff Njoroge <60646637+jeff283@users.noreply.github.com>
2025-12-09 23:56:51 +0300

    Add drizzle-zod for schema validation and enhance todos schema with zod integration

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,12 +1,23 @@
 import { pgTable, uuid, text, boolean, timestamp } from 'drizzle-orm/pg-core'
+import {
+  createSelectSchema,
+  createInsertSchema,
+  createUpdateSchema,
+} from 'drizzle-zod'
 
 export const todos = pgTable('todos', {
   id: uuid('id').primaryKey().defaultRandom(),
   name: text('name').notNull(),
-  isComplete: boolean().notNull(),
+  isComplete: boolean().notNull().default(false),
   createdAt: timestamp({ withTimezone: true }).defaultNow().notNull(),
   updatedAt: timestamp({ withTimezone: true })
     .defaultNow()
     .notNull()
     .$onUpdate(() => new Date()),
 })
+
+export const TodoSelectSchema = createSelectSchema(todos)
+export const TodoInsertSchema = createInsertSchema(todos)
+export const TodoUpdateSchema = createUpdateSchema(todos)
+export type Todo = typeof todos.$inferSelect
+export type NewTodo = typeof todos.$inferInsert
