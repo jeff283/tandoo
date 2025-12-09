c4153b2c76e8745bfd0ab54ce1b3457aebac6455
Jeff Njoroge <60646637+jeff283@users.noreply.github.com>
2025-12-09 23:16:09 +0300

    Scaffold the basic add new todo form

--- src/routes/todos/new/index.tsx
+++ src/routes/todos/new/index.tsx
@@ -1,9 +1,40 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
+import { ArrowLeftIcon } from 'lucide-react'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
 
 export const Route = createFileRoute('/todos/new/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/todos/new/"!</div>
+  return (
+    <div className=" container mx-auto py-6 space-y-2">
+      <Link to="/" className="flex">
+        <ArrowLeftIcon />
+        <span>Back</span>
+      </Link>
+      <Card>
+        <CardHeader>
+          <CardTitle>Add New Todo</CardTitle>
+          <CardDescription>
+            Create a new task to add to your todo list
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <TodoForm />
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
+
+const TodoForm = () => {
+  return <div>Todo Form</div>
 }
